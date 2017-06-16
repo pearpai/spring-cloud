d171bc7dbe88b7538b8c33df72f21d76a8b5da3a
pearpai <469901870@qq.com>
2017-06-16 15:59:50 +0800

    完成 config 服务功能

--- bq-client-service/src/main/java/com/bq/BqClientApplication.java
+++ bq-client-service/src/main/java/com/bq/BqClientApplication.java
@@ -2,14 +2,14 @@ package com.bq;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * app
  * Created by wuyunfeng on 2017/6/14.
  */
-@EnableDiscoveryClient
-@SpringBootApplication
+@SpringCloudApplication
 public class BqClientApplication {
 
     public static void main(String[] args) {
