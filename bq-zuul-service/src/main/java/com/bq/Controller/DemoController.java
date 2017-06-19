0589383d5e56ed85e2aef962d6d115bde80435ae
pearpai <469901870@qq.com>
2017-06-19 09:23:01 +0800

    添加网关功能

---
+++ bq-zuul-service/src/main/java/com/bq/Controller/DemoController.java
@@ -0,0 +1,35 @@
+package com.bq.Controller;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.*;
+
+
+/**
+ *
+ * Created by wuyunfeng on 2017/6/14.
+ */
+@RestController
+public class DemoController {
+
+    private final Logger logger = Logger.getLogger(getClass());
+
+    private final DiscoveryClient discoveryClient;
+
+    @Autowired
+    public DemoController(DiscoveryClient discoveryClient) {
+        this.discoveryClient = discoveryClient;
+    }
+
+
+    @GetMapping(value = "/add")
+    public Integer add(@RequestParam Integer a, @RequestParam Integer b) {
+
+        Integer r = a - b;
+        logger.info("server 1 : " + r );
+        return r;
+    }
+
+}
